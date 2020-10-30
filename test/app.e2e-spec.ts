df102b503661c1abf31852b5c5e4331bcc8aaca5
Donghwan Seo <tonky0110@gmail.com>
2020-10-30 22:13:04 +0900

    4.1 e2e test: movies/:id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -46,5 +53,31 @@ describe('AppController (e2e)', () => {
         .delete('/movies')
         .expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it.todo('GET'); // 테스트를 만들어야하는 항목.
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404);
+    });
+    it.todo('DELETE'); // 테스트를 만들어야하는 항목.
+    // it('GET 200', () => {
+    //   return request(app.getHttpServer())
+    //     .get('movies/1')
+    //     .expect(200);
+    // });
+    // it.todo('PATCH'); // 테스트를 만들어야하는 항목.
+    // it('GET 200', () => {
+    //   return request(app.getHttpServer())
+    //     .get('movies/1')
+    //     .expect(200);
+    // });
   });
 });
